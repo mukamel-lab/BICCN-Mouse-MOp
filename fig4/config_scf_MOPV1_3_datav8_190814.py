bee9290eeafbce69c0196de154fc80d45d3db52d
Fangming Xie <fmxie1993@gmail.com>
2019-09-19 13:10:07 -0700

    data_freeze

---
+++ fig4/config_scf_MOPV1_3_datav8_190814.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""An example configuration file
+"""
+import sys
+import os
+
+# # Configs  
+name = 'mopv1_exc_2datasets_datav8_190814'
+outdir = '/cndd/fangming/CEMBA/data/MOp_all/results'
+output_pcX_all = outdir + '/pcX_all_{}.npy'.format(name)
+output_cells_all = outdir + '/cells_all_{}.npy'.format(name)
+output_imputed_data_format = outdir + '/imputed_data_{}_{{}}.npy'.format(name)
+output_clst_and_umap = outdir + '/intg_summary_{}.tsv'.format(name)
+output_figures = outdir + '/figures/{}_{{}}.{{}}'.format(name)
+
+
+DATA_DIR = '/cndd/fangming/CEMBA/data/MOp_all/organized_mop_v1_exc'
+# fixed dataset configs
+sys.path.insert(0, DATA_DIR)
+from __init__datasets import *
+
+meta_f = os.path.join(DATA_DIR, '{0}_metadata.tsv')
+hvftrs_f = os.path.join(DATA_DIR, '{0}_hvfeatures.{1}')
+hvftrs_gene = os.path.join(DATA_DIR, '{0}_hvfeatures.gene')
+hvftrs_cell = os.path.join(DATA_DIR, '{0}_hvfeatures.cell')
+
+mods_selected = [
+    # 'snmcseq_gene',
+    # 'snatac_gene',
+    'smarter_cells',
+    # 'smarter_nuclei',
+    # '10x_cells', 
+    # '10x_cells_v3',
+    # '10x_nuclei_v3',
+    # '10x_nuclei_v3_Macosko',
+    'smarter-cells-v1',
+    ]
+
+# features_selected = ['smarter-cells-v1', 'smarter_cells']
+features_selected = ['smarter-cells-v1']
+
+# check features
+for features_modality in features_selected:
+    assert (features_modality in mods_selected)
+
+# within modality
+ps = {'mc': 0.9,
+      'atac': 0.1,
+      'rna': 0.7,
+     }
+drop_npcs = {
+      'mc': 0,
+      'atac': 0,
+      'rna': 0,
+     }
+
+# across modality
+cross_mod_distance_measure = 'correlation' # cca
+knn = 10 
+relaxation = 3 
+n_cca = 30
+
+# PCA
+npc = 50
+
+# clustering
+k = 30
+resolutions = [0.1, 1, 2, 4]
+# umap
+umap_neighbors = 30
+min_dist = 0.5
